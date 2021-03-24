daa46b54660648eb3086f5a37ce6e531a2654b9e
EgorKraevTransferwise <egor.kraev@transferwise.com>
2021-03-24 09:31:36 +0000

    The kafka example notebook works

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # clickhouse_utils
 A collection of Clickhouse-related utilities
+
+To spin up a docker-compose cluster with Kafka and Clickhouse, run `docker-compose up -d` in this directory. 
+Warning: the ports it exposes won't be visible outside of the machine it runs on unless you port map with ssh or similar.
+
+To bring up a standalone Clickhouse instance visible outside of this machine, run 
+`docker run -d --network=host --name clickhouse-server --ulimit nofile=262144:262144 -p 8123:8123 -p 9000:9000 --volume=storage:/var/lib/clickhouse yandex/clickhouse-server`
---
+++ src/clickhouse_utils/schema.py
@@ -0,0 +1,56 @@
+from typing import List, Union, Sequence
+
+import pandas as pd
+import numpy as np
+
+type_dict = {
+    np.dtype("int64"): "Int64",
+    np.dtype("int32"): "Int32",
+    np.dtype("O"): "String",
+    np.dtype("float64"): "Float64",
+    np.dtype("float32"): "Float32",
+    np.dtype("<M8[ns]"): "DateTime",
+}
+
+
+def normalize_types(x: np.dtype):
+    if x == np.dtype("float64"):
+        return np.float32
+    else:
+        return x
+
+
+def make_clickhouse_schema(
+    dtypes: pd.Series,
+    table_name: str,
+    order_by: Union[str, Sequence[str]],
+    high_granularity: Sequence = (),
+):
+
+    cols = []
+    for cname, ctype in dtypes.iteritems():
+        cht = type_dict[ctype]
+        if cht == "String" and cname not in high_granularity:
+            cht = "LowCardinality(String)"
+        cols.append(f"    `{cname}` {cht},")
+
+    all_cols = "\n".join(cols)[:-1]  # drop the last comma
+
+    if len(order_by) == 1:
+        order_by = order_by[0]
+
+    if isinstance(order_by, str):
+        order_str = f"`{order_by}`"
+    else:
+        inner = ", ".join([f"`{c}`" for c in order_by])
+        order_str = f"({inner})"
+
+    schema = f"""
+                CREATE TABLE IF NOT EXISTS {table_name}
+                (
+                {all_cols}
+                )
+                ENGINE = MergeTree()
+                ORDER BY {order_str}
+            """
+    return schema
